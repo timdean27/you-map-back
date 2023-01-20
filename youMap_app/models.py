396c1ecef137a7f8d41646682fe3c02705ca3c66
vrkljam <vrkljam@earthlink.net>
2023-01-20 16:10:16 -0700

    made a model and migrated

--- youMap_app/models.py
+++ youMap_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Spot(models.Model):
+    post = models.CharField(max_length=500)
+    photo_url=models.TextField()
+    spot_time=models.DateField()
+    
+    def __str__(self):
+        return self.post
+    
+    
