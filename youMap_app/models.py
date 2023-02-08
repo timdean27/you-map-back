85d25ce010848ea6e3930b8eb6e8a834ecb45aab
vrkljam <vrkljam@earthlink.net>
2023-02-08 10:44:50 -0700

    added to model

--- youMap_app/models.py
+++ youMap_app/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 class User(models.Model):
     user_name=models.CharField(max_length=100)
+    email=models.EmailField(_(""), max_length=254)
+    password=models.CharField(max_length=100)
     
     def __str__(self):
         return self.user_name
